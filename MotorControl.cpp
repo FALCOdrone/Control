2ac2eba99deda5f6e2544fa204dde66fa36fa804
Joshuari <Joshuarizzello03@gmail.com>
2024-12-12 08:45:39 +0100

    Added setup and loop functions and a test only function to send commands

--- MotorControl.cpp
+++ MotorControl.cpp
@@ -1,19 +1,74 @@
 #include <Motor.h>
 
+    Servo MOTOR_FR;
+    Servo MOTOR_FL;
+    Servo MOTOR_RR;
+    Servo MOTOR_RL;
+
 void Motor::initialize(){
-    Servo ESC1;
-    Servo ESC2;
-    Servo ESC3;
-    Servo ESC4;
+
+    MOTOR_FR.writeMicroseconds(ZERO_THRUST);
+    MOTOR_FL.writeMicroseconds(ZERO_THRUST);
+    MOTOR_RR.writeMicroseconds(ZERO_THRUST);
+    MOTOR_RL.writeMicroseconds(ZERO_THRUST);
+}
+
+void setup(){
+    Serial.begin(9600);
+
+    // motor pin attachment and ESC and motors inizialization
+    MOTOR_FR.attach(ESC1);  //where as dfined int pindef.h, ESCn are the pins
+    MOTOR_FL.attach(ESC2); 
+    MOTOR_RR.attach(ESC3); 
+    MOTOR_RL.attach(ESC4);
+
+    Motor::initialize();
+}
+
+void loop(){
+    Serial.available() >0;
+    String command = Serial.readStringUntil('\n');
+    if (command == "start")
+    //{
+    //    Motor::driveMotors();
+    //}
+    parseCommand(command);
+    Motor::driveMotors(thrust);
 }
 
 void Motor::driveMotors(float thrust[]){
     //Motor FR (front right)
-    ESC1.writeMicroseconds(map(thrust[0], 0, 3560, 1000, 2000));
+    MOTOR_FR.writeMicroseconds(map(thrust[0], 0, 3560, 1000, 2000));
     //Motor FL
-    ESC2.writeMicroseconds(map(thrust[1], 0, 3560, 1000, 2000));
+    MOTOR_FL.writeMicroseconds(map(thrust[1], 0, 3560, 1000, 2000));
     //Motor RR (rear right)
-    ESC3.writeMicroseconds(map(thrust[2], 0, 3560, 1000, 2000));
+    MOTOR_RR.writeMicroseconds(map(thrust[2], 0, 3560, 1000, 2000));
     //Motor RL
-    ESC4.writeMicroseconds(map(thrust[3], 0, 3560, 1000, 2000));
+    MOTOR_RL.writeMicroseconds(map(thrust[3], 0, 3560, 1000, 2000));
+}
+
+void Motor::throttleCut(){
+    MOTOR_FR.writeMicroseconds(ZERO_THRUST);
+    MOTOR_FL.writeMicroseconds(ZERO_THRUST);
+    MOTOR_RR.writeMicroseconds(ZERO_THRUST);
+    MOTOR_RL.writeMicroseconds(ZERO_THRUST);
+}
+
+void parseCommand(String command) {  // send in serial something like thrust[1]=200
+    if (command.startsWith("thrust[")) {
+        // Trova l'indice
+        int startIndex = command.indexOf('[') + 1;
+        int endIndex = command.indexOf(']');
+        int motorIndex = command.substring(startIndex, endIndex).toInt();
+
+        // Trova il valore
+        int equalIndex = command.indexOf('=');
+        float value = command.substring(equalIndex + 1).toFloat();
+
+        // Aggiorna il vettore thrust
+        if (motorIndex >= 0 && motorIndex < 4) {
+            thrust[motorIndex] = value;
+        }
+
+    }
 }
--- motor.h
+++ motor.h
@@ -5,20 +5,13 @@
 #include <Servo.h>
 #include "common/pinDef.h"
 
-#define ESC1
-#define ESC2
-#define ESC3
-#define ESC4
-
+#define MAX_THRUST 2000
+#define ZERO_THRUST 1000
 
 class Motor {
-    private:
-        /* null */
     public:
         void initialize();
-        void driveMotors(float thrust[4]); // thrust vector function 
-        // respectively, float[0] = FR , float[1] = FL, float[2] = RR, float[3] = RL,
-        // where FR is front right, RR is rear right and so on
+        void driveMotors(float thrust[4]); // thrust vector function respectively, float[0] = FR , float[1] = FL, float[2] = RR, float[3] = RL, where FR is front right, RR is rear right and so on
         void throttleCut();
 };
 
