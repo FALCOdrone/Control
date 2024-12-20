6c4171539387619d26802ef5f2bb57200d275520
Joshuari <Joshuarizzello03@gmail.com>
2024-12-20 15:04:42 +0100

    header and control files updated and debugged.

--- MotorControl.cpp
+++ MotorControl.cpp
@@ -1,90 +1,60 @@
 #include <Motor.h>
 
-unsigned long lastMillis = 0;
+float thrust[4] = {0, 0, 0, 0};
 
-Servo MOTOR_FR;
-Servo MOTOR_FL;
-Servo MOTOR_RR;
-Servo MOTOR_RL;
 
-void Motor::initialize(){
-    MOTOR_FR.writeMicroseconds(ZERO_THRUST);
-    MOTOR_FL.writeMicroseconds(ZERO_THRUST);
-    MOTOR_RR.writeMicroseconds(ZERO_THRUST);
-    MOTOR_RL.writeMicroseconds(ZERO_THRUST);
+
+void setup() {
+    Serial.begin(9600);
+
+    // Motor inizialization (pin attachment and ESC inizialization)
+    MOTOR_FR.initialize();
+    MOTOR_FL.initialize();
+    MOTOR_RR.initialize();
+    MOTOR_RL.initialize();
 }
 
-void Motor::driveMotors(float thrust[]){
-    //Motor FR (front right)
-    MOTOR_FR.writeMicroseconds(map(thrust[0], 0, 3560, 1000, 2000));
-    //Motor FL
-    MOTOR_FL.writeMicroseconds(map(thrust[1], 0, 3560, 1000, 2000));
-    //Motor RR (rear right)
-    MOTOR_RR.writeMicroseconds(map(thrust[2], 0, 3560, 1000, 2000));
-    //Motor RL
-    MOTOR_RL.writeMicroseconds(map(thrust[3], 0, 3560, 1000, 2000));
-}
-
-void parseCommand(String command) {  // send in serial something like "thrust[1]=200" or "start test"
+void parseCommand(String command) {
     if (command.startsWith("thrust[")) {
-        // finds the motor index (FR=0, FL=1, RR = 2, RL = 3)
+        // Trova l'indice del motore
         int startIndex = command.indexOf('[') + 1;
         int endIndex = command.indexOf(']');
         int motorIndex = command.substring(startIndex, endIndex).toInt();
 
-        // find the value in the string and converts it and saves it to float variable value
+        // Trova il valore del thrust
         int equalIndex = command.indexOf('=');
         float value = command.substring(equalIndex + 1).toFloat();
 
-        // thrust vector update
+        // Aggiorna il vettore thrust
         if (motorIndex >= 0 && motorIndex < 4) {
             thrust[motorIndex] = value;
         }
-    }
-    else if (command == "start test")
-    {
-        int k = 0;
-        while (k <= 1)
-        {
-            thurst[0] = k*100;
-            Motor::driveMotors(thrust);
+    } else if (command == "start test") {
+        for (int k = 0; k <= 1; k++) {
+            thrust[0] = k * 100;
+            MOTOR_FR.driveMotors(thrust[0]);
+            MOTOR_FL.driveMotors(thrust[1]);
+            MOTOR_RR.driveMotors(thrust[2]);
+            MOTOR_RL.driveMotors(thrust[3]);
             delay(300);
-            k+=1;
         }
+    } else if (command == "stop") {
+        MOTOR_FR.throttleCut();
+        MOTOR_FL.throttleCut();
+        MOTOR_RR.throttleCut();
+        MOTOR_RL.throttleCut();
     }
-    else
-    {
-        Motor::throttleCut();
-    }
 }
 
-void Motor::throttleCut(){
-    MOTOR_FR.writeMicroseconds(ZERO_THRUST);
-    MOTOR_FL.writeMicroseconds(ZERO_THRUST);
-    MOTOR_RR.writeMicroseconds(ZERO_THRUST);
-    MOTOR_RL.writeMicroseconds(ZERO_THRUST);
-}
-
-
-void setup(){
-    Serial.begin(9600);
-
-    // motor pin attachment and ESC and motors inizialization
-    MOTOR_FR.attach(ESC1);  //where as dfined int pindef.h, ESCn are the pins
-    MOTOR_FL.attach(ESC2); 
-    MOTOR_RR.attach(ESC3); 
-    MOTOR_RL.attach(ESC4);
-
-    Motor::initialize();
-}
-
-void loop(){
+void loop() {
     if (Serial.available() > 0) {
         String command = Serial.readStringUntil('\n');
         parseCommand(command);
-        Motor::driveMotors(thrust);
+
+        // Aggiorna i motori con il vettore thrust
+        MOTOR_FR.driveMotors(thrust[0]);
+        MOTOR_FL.driveMotors(thrust[1]);
+        MOTOR_RR.driveMotors(thrust[2]);
+        MOTOR_RL.driveMotors(thrust[3]);
     }
 }
-
-
-
--- motor.h
+++ motor.h
@@ -9,10 +9,36 @@
 #define ZERO_THRUST 1000
 
 class Motor {
-    public:
-        void initialize();
-        void driveMotors(float thrust[4]); // thrust vector function respectively, float[0] = FR , float[1] = FL, float[2] = RR, float[3] = RL, where FR is front right, RR is rear right and so on
-        void throttleCut();
+  private:
+    Servo motor; // Servo object to control the motor
+    int pin;     // Motor pin
+
+  public:
+    Motor(int pin) {
+        pin = pinNumber;
+    }
+
+    // motor inizialization
+    void initialize() {
+        motor.attach(pin);
+        motor.writeMicroseconds(ZERO_THRUST);
+    }
+
+    // Motors drive function
+    void driveMotors(float value) {
+        int pulseWidth = map(value, 0, 3560, 1000, 2000);
+        motor.writeMicroseconds(pulseWidth);
+    }
+
+    void throttleCut() {
+        motor.writeMicroseconds(ZERO_THRUST);
+    }
 };
+
+// Motor objects declaration and corresponding motor pin
+Motor MOTOR_FR(ESC1);
+Motor MOTOR_FL(ESC2);
+Motor MOTOR_RR(ESC3);
+Motor MOTOR_RL(ESC4);
 
 #endif
